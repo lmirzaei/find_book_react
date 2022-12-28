ed06a148ad867f6ca7be10714144edbb0b9923b8
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2022-12-28 10:55:53 -0800

    Display list of books titles fetched from API

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular(){
+const [popular, setPopular] = useState([]);
+
     useEffect(() => {
         getPopular();
     },[]);
@@ -10,9 +12,19 @@ function Popular(){
         const api = await fetch('https://www.googleapis.com/books/v1/volumes?q=quilting');
         const data = await api.json();
         console.log(data);
-    }
-    return (
-        <div>Popular</div>
-    )
+
+        setPopular(data.items);
+    };
+
+    return  <div>
+            { popular.map ( (book) => {
+                        return (<div key={book.id}>
+                            <p>
+                                {book.volumeInfo.title}
+                            </p>
+                        </div>)
+            })}
+        </div>;
+    
 }
 export default Popular
