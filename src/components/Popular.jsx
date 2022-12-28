28ff17cd152fe7707686610099884b51328facb8
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2022-12-28 11:51:42 -0800

    Add style for popular books and display image for them

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -19,15 +19,41 @@ const [popular, setPopular] = useState([]);
 
     return  <div>
             { popular.map ( (book) => {
-                        return (<div key={book.id}>
-                            <p>
-                                {book.volumeInfo.title}
-                            </p>
-                        </div>)
+                        return (
+                        // <div key={book.id}>
+                        //     <p>
+                        //         {book.volumeInfo.title}
+                        //     </p>
+                        // </div>
+                        <Wrapper>
+                            <h3>Popular Books</h3>
+                            {popular.map((book) => {
+                                return(
+                                    <Card>
+                                        <p>{book.volumeInfo.title}</p>
+                                        <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title} />
+                                    </Card>
+                                );
+                            })}
+                        </Wrapper>
+                        )
             })}
         </div>;
     
 }
 
 const Wrapper = styled.div`margin: 4rem 0 rem;`;
+
+const Card = styled.div` 
+        min-height: 25 rem;
+        border-radius: 3rem;
+        background: #f2bbe6;
+        padding: 1rem;
+        margin: 1rem;
+        overflow: hidden;
+        img{
+            border-radius:1rem;
+        }
+`;
+
 export default Popular
