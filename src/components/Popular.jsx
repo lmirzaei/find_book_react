ad472e9afa904204acffc4418f1a3063a93bbb42
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2022-12-27 21:12:11 -0800

    Add function to fetch data from API

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 function Popular(){
+    useEffect(() => {
+        getPopular();
+    },[]);
+
+    const getPopular = async() =>
+    {
+        const api = await fetch('https://www.googleapis.com/books/v1/volumes?q=quilting');
+        const data = await api.json();
+        console.log(data);
+    }
     return (
         <div>Popular</div>
     )
