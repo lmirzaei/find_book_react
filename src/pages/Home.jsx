87e87f88c78d74c0c8f1fbdde3fe23fd74f97e3d
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2022-12-29 15:24:19 -0800

    Add pages.jsx for navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Trending from "../components/Trending";
 import Popular from "../components/Popular";
 
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
