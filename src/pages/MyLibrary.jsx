6605007327e940c504eb6e211586b4d32fe9233c
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2022-12-31 17:33:43 -0800

    Add parameter and Grid for menu bar

--- src/pages/MyLibrary.jsx
+++ src/pages/MyLibrary.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 function MyLibrary() {
-  return (
-    <div>MyLibrary Test</div>
-  )
+    const [mylibrary, setMyLibrary] = useState([]);
+    let params = useParams();
+
+    const getMyLibrary = async (name) => {
+        const api = await fetch('https://www.googleapis.com/books/v1/volumes?q=${name}');
+        const data = await api.json();
+        setMyLibrary(data.items);
+    };
+
+    // use effect invokes the `getMyLibrary` function.
+    useEffect(() => {
+        getMyLibrary(params.type)
+        console.log(params.type);
+    }, [params.type]);
+
+    return (
+        <div>MyLibrary Test</div>
+    )
 }
+
+const Grid = styled.div`
+    display:grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3rem;
+}`;
+
+const Card = styled.div`
+    img{
+        width:100%;
+        border-radius:2rem;
+    }
+    a{
+        text-decoration:none;
+    }
+    h4{
+        text-align: center;
+        padding:1rem;
+    }
+`;
 
 export default MyLibrary
