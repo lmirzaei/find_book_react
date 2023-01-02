44ca834c71c5b3f1118d5a1211effddfded8b923
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2023-01-02 11:41:55 -0800

    Display result of search in Searched page

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 function Searched() {
-  return (
-    <div>Searched Page is Here</div>
-  )
+  const [searchedBooks, setSearchedBookks] = useState([]);
+    let params = useParams();
+
+    const getSearched = async (name) => {
+        const api = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${name}`);
+        const data = await api.json();
+        setSearchedBookks(data.items);
+    };
+
+    // use effect invokes the `getSearched` function.
+    useEffect(() => {
+      getSearched(params.search)
+    }, [params.search]);
+
+    return (
+        <div>
+            {searchedBooks.map((book) => {
+                return (
+                    <div key={book.id}>
+                        <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title} />
+                        <h4>{book.volumeInfo.title}</h4>
+                    </div>
+                );
+            })}
+        </div>
+    )
 }
 
 export default Searched
